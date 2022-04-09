7fd2c642f9fc90197f13c87618e452b8d6697a24
Rabbiya-Noor <rabbiyanoor86@gmail.com>
2022-04-09 14:04:10 +0500

    testfile for sbi_probe_extension

---
+++ code/src/test_srcs/test__sbi_probe_extension/test__sbi_probe_extension.c
@@ -0,0 +1,44 @@
+#include "test_macros.h"
+
+int test_case(void);
+
+/**
+ * @brief interrupt handler function that runs in S-mode. Not used in this test
+ * 
+ */
+__attribute__((interrupt("supervisor")))
+void s_mode_trap(void) {
+  uint64_t cause = read_csr(scause);
+
+  switch (cause) {
+    default:
+      break;
+  }
+}
+
+/**
+ * @brief Test case example checks for illegal instruction exception
+ * 
+ * @return int 
+ */
+int test_case(void) {
+  struct sbiret get_spec = sbi_probe_extension(EID_10);
+
+  if(get_spec.value == 1)
+    {
+        exit_test(6);
+        if(get_spec.error == SBI_SUCCESS)
+        {
+            exit_test(TEST_PASS);
+        }
+     }
+
+  /*sbi_spec_version val;
+  val.full_ver = get_spec.value;
+  if (val.major_ver != 0)
+    exit_test(TEST_FAIL);
+  if (val.minor_ver != 2)
+    exit_test(TEST_FAIL);*/
+else
+  {exit_test(10);}
+}
--- testlists/test__sbi_get_spec_version.sh
+++ testlists/test__sbi_get_spec_version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 source export.sh
-make TEST_DIR="code/src/test_srcs/test__sbi_get_spec_version" CFLAGS="-DDEBUG"
+make TEST_DIR="code/src/test_srcs/test__sbi_probe_extension" CFLAGS="-DDEBUG"
 make spike
