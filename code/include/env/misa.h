c965f1fe3ed2ac2d5a0bb143de793e07dcf033d7
Muhammad_ahad <muhammad.ahad0497@gmail.com>
2022-04-09 11:30:30 +0500

    All comments are resolved for misa.h

--- code/include/env/misa.h
+++ code/include/env/misa.h
@@ -1,8 +1,7 @@
 #ifndef RISCV_MISA_H
 #define RISCV_MISA_H
 
-#define MISA_EXTENTIONS_SHIFT           0
-#define MISA_ZERO_SHIFT                 26
+#define MISA_EXTENSIONS_SHIFT           0
 #define MISA_MXL_SHIFT                  61
 #define MISA_A_SHIFT                    0
 #define MISA_C_SHIFT                    2
@@ -13,15 +12,14 @@
 #define MISA_I_SHIFT                    8
 #define MISA_M_SHIFT                    12
 #define MISA_N_SHIFT                    13
-#define MISA_P_SHIFT                    15
+#define MISA_P_SHIFT                    15         
 #define MISA_Q_SHIFT                    16
 #define MISA_S_SHIFT                    18
 #define MISA_U_SHIFT                    20
 #define MISA_V_SHIFT                    21
 #define MISA_X_SHIFT                    23
 
-#define MISA_EXTENTIONS_MASK            0x0000000001FFFFFFull
-#define MISA_ZERO_MASK                  0x0000000000000000ull
+#define MISA_EXTENSIONS_MASK            0x0000000001FFFFFFull
 #define MISA_MXL_MASK                   0xC000000000000002ull
 #define MISA_A_MASK                     0x0000000000000001ull
 #define MISA_C_MASK                     0x0000000000000004ull
