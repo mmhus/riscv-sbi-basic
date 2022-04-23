067a69c784b6a8692cc30f1ad1833fc8df028c7d
hassanraza164 <hassain18071999@gmail.com>
2022-04-23 10:39:50 +0500

    section 3.6 and 3.7

---
+++ code/src/test/test__sbi_get_mimpid/test__sbi_get_mimpid.c
@@ -0,0 +1,33 @@
+#include "test_macros.h"
+
+int test_case(void);
+
+/**
+ * @brief interrupt handler function that runs in S-mode. Not used in this test
+ * 
+ */
+__attribute__((interrupt("supervisor")))
+void s_mode_trap(void) {
+  uint64_t cause = read_csr(scause);
+
+  switch (cause) {
+    default:
+      break;
+  }
+}
+
+/**
+ * @brief Test case example checks for mimpid
+ * 
+ * @return int 
+ */
+
+int test_case(void) {
+    struct sbiret get_mimpid = sbi_get_mimpid();
+
+    if (get_mimpid.error != SBI_SUCCESS)
+      exit_test(TEST_FAIL);
+
+    if (get_mimpid.value == 0)
+       exit_test(TEST_PASS);
+}
